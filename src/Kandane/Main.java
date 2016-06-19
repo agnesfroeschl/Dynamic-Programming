547ebb7e51fe49fbdda8ab220a1a3f7a205e7483
agnesfroeschl <agnes.froeschl@gmx.at>
2016-06-19 10:57:47 +0200

    cleanup names

--- src/Kandane/Main.java
+++ src/Kandane/Main.java
@@ -7,16 +7,19 @@ public class Main {
 
         int[] array = {31, -41, 59, 26, -53, 58, 97, -93, -23, 84};
         KadaneMaxSubarray k = new KadaneMaxSubarray();
-        int sum = k.maxSubarraySum(array);
+        int sum = k.maxSubArray(array);
         System.out.println("sum: " + sum);
 
-        sum = k.maxSubarrayArray(array);
-        System.out.println("sum: " + sum);
+        sum = k.maxSubArray1(array);
+        System.out.println("sum1: " + sum);
+
+        sum = k.maxSubArray2(array);
+        System.out.println("sum2: " + sum);
 
 
         int[] array1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         KadaneMaxSubarray k1 = new KadaneMaxSubarray();
-        int sum1 = k.maxSubarraySum(array1);
+        int sum1 = k.maxSubArray(array1);
         System.out.println("sum: " + sum1);
 
     }
