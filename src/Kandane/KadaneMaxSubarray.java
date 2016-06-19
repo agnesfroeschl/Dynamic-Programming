a92a57b3c1ea80326b513d2eb2a251441562b17a
agnesfroeschl <agnes.froeschl@gmx.at>
2016-06-19 10:57:28 +0200

    new alternative for Kandane algorithm

--- src/Kandane/KadaneMaxSubarray.java
+++ src/Kandane/KadaneMaxSubarray.java
@@ -23,7 +23,7 @@ public class KadaneMaxSubarray {
      * @param array
      * @return max sum of all integers in the array
      */
-    public int maxSubarraySum(int[] array) {
+    public int maxSubArray(int[] array) {
         maxEndingHere = maxSoFar = 0;
         for (int i : array) {
             maxEndingHere = Math.max(i, maxEndingHere + i);
@@ -40,7 +40,7 @@ public class KadaneMaxSubarray {
      * @param array
      * @return array containing all itegers of the max subarray
      */
-    public int maxSubarrayArray(int[] array) {
+    public int maxSubArray1(int[] array) {
         maxEndingHere = maxSoFar = array[0];
         int[] maxSum = new int[array.length];
 
@@ -49,5 +49,38 @@ public class KadaneMaxSubarray {
             maxSoFar = Math.max(maxSoFar, maxSum[i]);
         }
         return maxSoFar;
+    }
+
+
+
+    public int maxSubArray2(int[] A) {
+        int max = A[0];
+        int[] sum = new int[A.length];
+        sum[0] = A[0];
+
+        int begin = 0;
+        int end = 0;
+
+        for (int i = 1; i < A.length; i++) {
+
+            int newBegin = begin;
+            int newEnd = end;
+
+            if (A[i] > sum[i - 1] + A[i]) {
+                sum[i] = A[i];
+                newBegin = i;
+                newEnd = i;
+            } else {
+                sum[i] = sum[i - 1] + A[i];
+                newEnd = i;
+            }
+
+            if (sum[i] > max) {
+                max = sum[i];
+                begin = newBegin;
+                end = newEnd;
+            }
+        }
+        return max;
     }
 }
